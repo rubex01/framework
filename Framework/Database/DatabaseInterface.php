873b8f6c62160fcb7a749fe04c4d7ce6b471b0e3
rubex01 <40766757+rubex01@users.noreply.github.com>
2020-09-16 11:48:56 +0200

    added documentation for database interface

--- Framework/Database/DatabaseInterface.php
+++ Framework/Database/DatabaseInterface.php
@@ -3,7 +3,18 @@
 namespace Framework\Database;
 
 interface DatabaseInterface {
+    /**
+     * Makes database connection and returns it
+     * 
+     * @return object
+     */
     public function makeDatabaseConnection();
 
+    /**
+     * In case of connection error trigger exception
+     * 
+     * @param string $causeOfException
+     * @return void
+     */
     public function connectionError(string $causeOfException);
 }
